62fca7ad8d4328ab0b43f5a910d4f1d8dc7efee5
Oglo12 <jacksonnovak@proton.me>
2024-09-12 15:36:14 -0500

    Improvements to clock.

--- waybar/scripts/clock_json.sh
+++ waybar/scripts/clock_json.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+export LIFETIME=2000 # I just watched 'I Want To Eat Your Pancreas' so... I am a bit emotional right now... (I rate it a 10/10 for sure!)
+
 json_field () {
     echo -n "\"$1\":\"$2\""
 
@@ -10,11 +12,19 @@ time_24h="$(date '+%H:%M')"
 time_12h="$(date '+%I:%M %p')"
 time_date="$(date '+%A, %B %d, %Y')"
 
-if [[ "$1" == "click-left" ]]; then
-    notify-send -e "Uptime is..." "$(uptime -p | sed 's/up //')"
+hit_a_click=1
+
+if [[ "$1" == "click-middle" ]]; then
+    notify-send -e -t "${LIFETIME}" "Uptime is..." "$(uptime -p | sed 's/up //')"
+elif [[ "$1" == "click-left" ]]; then
+    notify-send -e -t "${LIFETIME}" "The date is..." "$time_date"
 elif [[ "$1" == "click-right" ]]; then
-    notify-send -e "The date is..." "$time_date"
+    notify-send -e -t "${LIFETIME}" "12h -> 24h is..." "$time_12h -> $time_24h"
+else
+    hit_a_click=0
 fi
+
+[[ $hit_a_click == 1 ]] && exit
 
 json_tooltip="$time_date"
 json_text="$time_12h"
