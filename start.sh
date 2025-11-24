affcb168d5e658a15a7072e10b507ab236ebf83f
= <seppi@neureiter.bayern>
2025-11-24 19:07:14 +0100

    work save

---
+++ start.sh
@@ -0,0 +1,10 @@
+pkill swww
+swww-daemon &
+swww img background.jpg
+
+pkill eww
+$HOME/.config/eww/start.sh
+
+pipewire
+
+hyprpm reload
--- swaync/scripts/notify_count.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-notif_count="$(swaync-client -c)"
-
-notify-send -e -t 2000 "You have..." "${notif_count} notifications."
